b202445d43a0d388d99b005422eefc1d1e68db85
artaddon <sebarsomo@gmail.com>
2025-05-19 16:17:36 -0500

    feat: Agrego la url para el inicio

--- src/main/java/co/com/AutoFusdec/userinterface/InicioFusdec.java
+++ src/main/java/co/com/AutoFusdec/userinterface/InicioFusdec.java
@@ -1,4 +1,9 @@
 package co.com.AutoFusdec.userinterface;
 
-public class InicioFusdec {
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("http://localhost:5173/home")
+public class InicioFusdec extends PageObject {
 }
