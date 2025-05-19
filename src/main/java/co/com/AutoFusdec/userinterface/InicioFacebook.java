cebc852cab2566d8835682662e76b7959e542a31
Andres Pinzon <andrespinzonxd@gmail.com>
2025-05-19 15:38:55 -0500

    feat: levantamiento de entorno

---
+++ src/main/java/co/com/AutoFusdec/userinterface/InicioFacebook.java
@@ -0,0 +1,9 @@
+package co.com.AutoFusdec.userinterface;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl(value = "https://www.facebook.com/")
+
+public class InicioFacebook extends PageObject {
+}
