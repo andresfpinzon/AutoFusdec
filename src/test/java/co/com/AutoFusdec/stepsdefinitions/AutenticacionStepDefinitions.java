bcf6c73698b2c69b04b40dfd273790a5c5e71ae6
Andres Pinzon <andrespinzonxd@gmail.com>
2025-05-20 15:51:32 -0500

    correccion pasos de login

--- src/test/java/co/com/AutoFusdec/stepsdefinitions/AutenticacionStepDefinitions.java
+++ src/test/java/co/com/AutoFusdec/stepsdefinitions/AutenticacionStepDefinitions.java
@@ -9,20 +9,19 @@ import cucumber.api.java.es.Entonces;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class AutenticacionStepDefinitions {
-
     @Dado("^que el usuario se encuentra en la pagina de inicio de sesion de Fusdec$")
-    public void queElUsuarioSeEncuentraEnLaPaginaDeInicioDeSesionDeFacebook() {
+    public void queElUsuarioSeEncuentraEnLaPaginaDeInicioDeSesionDeFusdec() {
         theActorInTheSpotlight().wasAbleTo(AbrirPaginaFusdec.laPagina());
-
     }
+
 
     @Cuando("^ingrese las credenciales correctas \\(usuario y contrasena\\)$")
     public void ingreseLasCredencialesCorrectasUsuarioYContrasena(DataTable arg1) {
 
     }
 
-    @Entonces("^se debe verificar que el usuario haya sido autenticado correctamente y redirigido a su pagina de inicio de Facebook$")
-    public void seDebeVerificarQueElUsuarioHayaSidoAutenticadoCorrectamenteYRedirigidoASuPaginaDeInicioDeFacebook() {
+    @Entonces("^se debe verificar que el usuario haya sido autenticado correctamente y redirigido a la pagina de inicio de Fusdec$")
+    public void seDebeVerificarQueElUsuarioHayaSidoAutenticadoCorrectamenteYRedirigidoALaPaginaDeInicioDeFusdec() {
 
     }
 }
--- src/test/resources/features/login_fusdec.feature
+++ src/test/resources/features/login_fusdec.feature
@@ -7,10 +7,9 @@ Característica: Autenticacion en la pagina de Fusdec
   Para poder acceder al contenido y funcionalidades disponibles en mi cuenta.
 
   @autenticacion
-
   Escenario: Verificar la autenticacion exitosa en la pagina de Fusdec
     Dado que el usuario se encuentra en la pagina de inicio de sesion de Fusdec
     Cuando ingrese las credenciales correctas (usuario y contrasena)
       | usuarios | clave |
       | Fusdec@gmail.com | C0ntras3naFusd3cCE   |
-    Entonces se debe verificar que el usuario haya sido autenticado correctamente y redirigido a su pagina de inicio de Fusdec
+    Entonces se debe verificar que el usuario haya sido autenticado correctamente y redirigido a la pagina de inicio de Fusdec
