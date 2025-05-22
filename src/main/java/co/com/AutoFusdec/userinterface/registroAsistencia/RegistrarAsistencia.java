c30a7e680a5b5e439446c54c04689f07493b3d6a
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-05-22 15:39:17 -0500

    feat: Agrego

--- src/main/java/co/com/AutoFusdec/userinterface/registroAsistencia/RegistrarAsistencia.java
+++ src/main/java/co/com/AutoFusdec/userinterface/registroAsistencia/RegistrarAsistencia.java
@@ -7,4 +7,5 @@ import org.openqa.selenium.By;
 public class RegistrarAsistencia extends PageObject {
     public static Target MENU_DESPLEGABLE = Target.the("Ver menú desplegable").located(By.xpath("//*[@id=\"root\"]/header/div/button/svg"));
 
+    public static Target PAGE_ASISTENCIA = Target.the("Pagina Asistencia").located(By.xpath("/html/body/div[2]/div[3]/div/ul/li[5]/div[2]/span"));
 }
