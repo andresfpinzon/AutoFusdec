0e90a259382cbd1b454e1a5ae4fd86d278eed4c0
Julian Rivera(VALHEMSING) <juli.rivera.quintero@gmail.com>
2025-05-22 15:36:20 -0500

    feat: Agrego target para el menu desplegable

--- src/main/java/co/com/AutoFusdec/userinterface/registroAsistencia/RegistrarAsistencia.java
+++ src/main/java/co/com/AutoFusdec/userinterface/registroAsistencia/RegistrarAsistencia.java
@@ -1,6 +1,10 @@
 package co.com.AutoFusdec.userinterface.registroAsistencia;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
 
 public class RegistrarAsistencia extends PageObject {
+    public static Target MENU_DESPLEGABLE = Target.the("Ver menú desplegable").located(By.xpath("//*[@id=\"root\"]/header/div/button/svg"));
+
 }
