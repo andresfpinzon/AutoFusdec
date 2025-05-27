ad6b103f4026ac00183b7e656d327f39753023be
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-05-27 18:27:42 -0500

    feat: finalizo model de brigada

--- src/main/java/co/com/AutoFusdec/models/brigadamodel/BrigadaModel.java
+++ src/main/java/co/com/AutoFusdec/models/brigadamodel/BrigadaModel.java
@@ -1,4 +1,29 @@
 package co.com.AutoFusdec.models.brigadamodel;
 
 public class BrigadaModel {
+    private String nombreBrigada;
+    private String ubicacionBrigada;
+
+    public BrigadaModel(String nombreBrigada, String ubicacionBrigada) {
+        this.nombreBrigada = nombreBrigada;
+        this.ubicacionBrigada = ubicacionBrigada;
+    }
+
+    public String getNombreBrigada() {
+        return nombreBrigada;
+    }
+
+    public void setNombreBrigada(String nombreBrigada) {
+        this.nombreBrigada = nombreBrigada;
+    }
+
+    public String getUbicacionBrigada() {
+        return ubicacionBrigada;
+    }
+
+    public void setUbicacionBrigada(String ubicacionBrigada) {
+        this.ubicacionBrigada = ubicacionBrigada;
+    }
+
+
 }
