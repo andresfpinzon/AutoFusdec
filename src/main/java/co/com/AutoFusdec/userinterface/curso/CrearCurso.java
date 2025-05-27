09737e233e1816e51b16fd311391bcd2ac4cac8b
SantiagoRYS <santiagorys2003@gmail.com>
2025-05-27 18:17:49 -0500

    feat: Agregar user interface para crear curso

--- src/main/java/co/com/AutoFusdec/userinterface/curso/CrearCurso.java
+++ src/main/java/co/com/AutoFusdec/userinterface/curso/CrearCurso.java
@@ -10,24 +10,19 @@ public class CrearCurso extends PageObject {
             .located(By.id("menuButtonLateral"));
     public static Target BTN_CURSO = Target.the("Boton de cursos ")
             .located(By.xpath("/html/body/div[2]/div[3]/div/ul/li[10]/div[2]/span"));
-    public static Target INPUT_DOCUMENTO = Target.the(" Ingreso del numero de documento")
-            .located(By.xpath("//*[@id=\"numeroDocumento\"]"));
-    public static Target INPUT_NOMBRE = Target.the("Ingreso del nombre")
-            .located(By.xpath("//*[@id=\"nombre\"]"));
-    public static Target INPUT_APELLIDO = Target.the("Ingreso del apellido ")
-            .located(By.xpath("//*[@id=\"apellido\"]"));
-    public static Target INPUT_CORREO = Target.the("Ingrese el correo")
-            .located(By.xpath("//*[@id=\"correo\"]"));
-    public static Target INPUT_PASSWORD = Target.the("Ingreso de la contraseña")
-            .located(By.xpath("//*[@id=\"password\"]"));
-    public static Target BTN_ROL = Target.the("Boton de enviar")
-            .located(By.xpath("//*[@id=\"root\"]/div/div/form/div[6]/div/div"));
-
-    public static Target BTN_ELECCION = Target.the("Boton de enviar")
-            .located(By.xpath("//*[@id=\":rj:\"]/li[1]/span[1]/input"));
+    public static Target INPUT_NOMBRE = Target.the(" Ingreso del nombre del curso")
+            .located(By.id("nombre"));
+    public static Target INPUT_DESCRIPCION = Target.the("Ingreso de la descripcion")
+            .located(By.id("descripcion"));
+    public static Target INPUT_INTENSIDAD = Target.the("Ingreso de la intencidad horaria")
+            .located(By.id("intensidadHoraria"));
+    public static Target BTN_FUNDACION = Target.the("Boton de fundacion")
+            .located(By.xpath("//*[@id=\"mui-component-select-fundacionId\"]"));
+    public static Target BTN_ELECCION = Target.the("Boton de seleccion")
+            .located(By.xpath("//*[@id=\":rf:\"]/li"));
     public static Target BTN_CREAR = Target.the("Boton de crear")
             .located(By.xpath("//*[@id=\"root\"]/div/div/form/div[7]/button"));
-    public static Target MENSAJE_EXITO = Target.the("Boton de crear")
+    /*public static Target MENSAJE_EXITO = Target.the("mensaje de exito")
             .located(By.xpath("//div[contains(@class,'MuiAlert-message') and text()='Usuario creado correctamente con sus roles']"));
-
+    */
 }
