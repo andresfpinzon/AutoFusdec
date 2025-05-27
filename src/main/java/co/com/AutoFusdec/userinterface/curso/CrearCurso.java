446c33e7500517728938980868feffabc8be314e
SantiagoRYS <santiagorys2003@gmail.com>
2025-05-27 17:57:39 -0500

    fix: Arreglar user interface de usuarios

--- src/main/java/co/com/AutoFusdec/userinterface/curso/CrearCurso.java
+++ src/main/java/co/com/AutoFusdec/userinterface/curso/CrearCurso.java
@@ -1,25 +1,25 @@
-package co.com.AutoFusdec.userinterface.usuario;
+package co.com.AutoFusdec.userinterface.curso;
 
 import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.targets.Target;
 
-public class CrearUsuario extends PageObject {
+public class CrearCurso extends PageObject {
 
     public static Target BTN_MENU = Target.the("Boton de menu desplegable")
             .located(By.id("menuButtonLateral"));
-    public static Target BTN_USUARIO = Target.the("Boton de usuarios")
-            .located(By.xpath("/html/body/div[2]/div[3]/div/ul/li[8]/div[2]/span"));
+    public static Target BTN_CURSO = Target.the("Boton de cursos ")
+            .located(By.xpath("/html/body/div[2]/div[3]/div/ul/li[10]/div[2]/span"));
     public static Target INPUT_DOCUMENTO = Target.the(" Ingreso del numero de documento")
-            .located(By.id("numeroDocumento"));
+            .located(By.xpath("//*[@id=\"numeroDocumento\"]"));
     public static Target INPUT_NOMBRE = Target.the("Ingreso del nombre")
-            .located(By.id("nombre"));
+            .located(By.xpath("//*[@id=\"nombre\"]"));
     public static Target INPUT_APELLIDO = Target.the("Ingreso del apellido ")
-            .located(By.id("apellido"));
+            .located(By.xpath("//*[@id=\"apellido\"]"));
     public static Target INPUT_CORREO = Target.the("Ingrese el correo")
-            .located(By.id("correo"));
+            .located(By.xpath("//*[@id=\"correo\"]"));
     public static Target INPUT_PASSWORD = Target.the("Ingreso de la contraseña")
-            .located(By.id("password"));
+            .located(By.xpath("//*[@id=\"password\"]"));
     public static Target BTN_ROL = Target.the("Boton de enviar")
             .located(By.xpath("//*[@id=\"root\"]/div/div/form/div[6]/div/div"));
 
