73eb3ba6e599360549084be229228b62c79d244e
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-05-27 18:13:44 -0500

    feat: finalizado el stepdefinitions y corregido el runner

--- src/test/java/co/com/AutoFusdec/runners/runnerBrigada/CrearBrigadaRunner.java
+++ src/test/java/co/com/AutoFusdec/runners/runnerBrigada/CrearBrigadaRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = "src/test/resources/features/crear_brigada.feature",
+        features = "src/test/resources/features/brigadaFeature/crear_brigada.feature",
         glue = {"co.com.AutoFusdec.stepsdefinitions", "co.com.AutoFusdec.utils.hooks"},
         snippets = SnippetType.CAMELCASE)
 
--- src/test/java/co/com/AutoFusdec/stepsdefinitions/stepsBrigada/CrearBrigadaStepDefinitions.java
+++ src/test/java/co/com/AutoFusdec/stepsdefinitions/stepsBrigada/CrearBrigadaStepDefinitions.java
@@ -1,4 +1,17 @@
 package co.com.AutoFusdec.stepsdefinitions.stepsBrigada;
 
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Entonces;
+
 public class CrearBrigadaStepDefinitions {
+    @Cuando("^estoy en la página de gestión de brigadas dar click en agregar, llenar campos y confirmar$")
+    public void estoyEnLaPáginaDeGestiónDeBrigadasDarClickEnAgregarLlenarCamposYConfirmar() {
+
+    }
+
+
+    @Entonces("^se debe verificar que el brigada se cree correctamente$")
+    public void seDebeVerificarQueElBrigadaSeCreeCorrectamente() {
+
+    }
 }
