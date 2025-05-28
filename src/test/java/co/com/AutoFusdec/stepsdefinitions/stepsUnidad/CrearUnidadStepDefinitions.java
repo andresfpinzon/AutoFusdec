49586b6ea06d79fc6aaa05764277774bd7c729cb
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-05-28 14:31:55 -0500

    feat: Agrego stepdefinitions de undiada

---
+++ src/test/java/co/com/AutoFusdec/stepsdefinitions/stepsUnidad/CrearUnidadStepDefinitions.java
@@ -0,0 +1,18 @@
+package co.com.AutoFusdec.stepsdefinitions.stepsUnidad;
+
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Dado;
+import cucumber.api.java.es.Entonces;
+
+public class CrearUnidadStepDefinitions {
+    @Cuando("^estoy en la página de gestión de Unidades dar click en agregar, ingresar \"([^\"]*)\" y confirmar$")
+    public void estoyEnLaPáginaDeGestiónDeUnidadesDarClickEnAgregarIngresarYConfirmar(String arg1) {
+
+    }
+
+
+    @Entonces("^se debe verificar que la unidad se cree correctamente$")
+    public void seDebeVerificarQueLaUnidadSeCreeCorrectamente() {
+
+    }
+}
