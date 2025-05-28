e4a8004ab0c65a18f422409b53ce5ce33746478c
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-05-28 13:28:41 -0500

    feat: Steps definitions de brigada para crear finalizado y correcto..

--- src/test/java/co/com/AutoFusdec/stepsdefinitions/stepsBrigada/CrearBrigadaStepDefinitions.java
+++ src/test/java/co/com/AutoFusdec/stepsdefinitions/stepsBrigada/CrearBrigadaStepDefinitions.java
@@ -1,17 +1,25 @@
 package co.com.AutoFusdec.stepsdefinitions.stepsBrigada;
 
+import co.com.AutoFusdec.models.brigadamodel.BrigadaModel;
+import co.com.AutoFusdec.questions.brigada.ValidacionCrearBrigada;
+import co.com.AutoFusdec.tasks.brigada.CrearBrigadaTask;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Entonces;
 
+import java.util.List;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+
 public class CrearBrigadaStepDefinitions {
     @Cuando("^estoy en la página de gestión de brigadas dar click en agregar, llenar campos y confirmar$")
-    public void estoyEnLaPáginaDeGestiónDeBrigadasDarClickEnAgregarLlenarCamposYConfirmar() {
-
+    public void estoyEnLaPáginaDeGestiónDeBrigadasDarClickEnAgregarLlenarCamposYConfirmar(List<BrigadaModel> datos) {
+        theActorInTheSpotlight().attemptsTo(CrearBrigadaTask.ingresarDatosBrigada(datos));
     }
 
 
     @Entonces("^se debe verificar que el brigada se cree correctamente$")
     public void seDebeVerificarQueElBrigadaSeCreeCorrectamente() {
-
+        theActorInTheSpotlight().should(seeThat(ValidacionCrearBrigada.creado()));
     }
 }
