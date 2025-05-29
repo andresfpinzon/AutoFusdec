1ae2e5dc56c15c25bd7227a354d3a19d2f76da95
SantiagoRYS <santiagorys2003@gmail.com>
2025-05-29 17:37:41 -0500

    feat: Agregar stepDefinitions para crear edicion

---
+++ src/test/java/co/com/AutoFusdec/stepsdefinitions/CrearEdicionStepDefinitions.java
@@ -0,0 +1,28 @@
+package co.com.AutoFusdec.stepsdefinitions;
+
+import co.com.AutoFusdec.models.usuario.FormularioUsuario;
+import co.com.AutoFusdec.questions.usuario.ValidacionCrearUsuario;
+import co.com.AutoFusdec.tasks.usuario.CreacionUsuario;
+import cucumber.api.PendingException;
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Entonces;
+
+import java.util.List;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+
+public class CrearEdicionStepDefinitions {
+
+    @Cuando("^este en la pagina de ediciones diligenciar correctamente el formulario y dar clic a crear edicion:$")
+    public void esteEnLaPaginaDeEdicionesDiligenciarCorrectamenteElFormularioYDarClicACrearEdicion() {
+        throw new PendingException();
+    }
+
+
+    @Entonces("^se debe verificar que la edicion se cree correctamente y se agregue a la lista de ediciones\\.$")
+    public void seDebeVerificarQueLaEdicionSeCreeCorrectamenteYSeAgregueALaListaDeEdiciones() {
+        throw new PendingException();
+    }
+
+}
