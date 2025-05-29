89765d87a6c6982377fb56cf550739b5a299b093
SantiagoRYS <santiagorys2003@gmail.com>
2025-05-28 22:22:26 -0500

    fix: Arreglar interface de pagina para que se habra en pantalla completa

--- src/main/java/co/com/AutoFusdec/userinterface/InicioFusdec.java
+++ src/main/java/co/com/AutoFusdec/userinterface/InicioFusdec.java
@@ -1,9 +1,14 @@
 package co.com.AutoFusdec.userinterface;
 
-
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebDriver;
 
 @DefaultUrl("http://localhost:5173/home")
 public class InicioFusdec extends PageObject {
+
+    public InicioFusdec(WebDriver driver) {
+        super(driver);
+        driver.manage().window().maximize(); // Maximiza la ventana al iniciar
+    }
 }
