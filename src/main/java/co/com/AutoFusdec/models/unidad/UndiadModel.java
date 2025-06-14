e19b5f3f40e7239bf203e9152333401d501294c8
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-14 16:44:10 -0500

    feat: Agrego modelo para manejo de datos de unidad

--- src/main/java/co/com/AutoFusdec/models/unidad/UndiadModel.java
+++ src/main/java/co/com/AutoFusdec/models/unidad/UndiadModel.java
@@ -1,2 +1,17 @@
-package co.com.AutoFusdec.models.unidad;public class UndiadModel {
+package co.com.AutoFusdec.models.unidad;
+
+public class UndiadModel {
+    private String nombreUnidad;
+
+    public String getNombreUnidad() {
+        return nombreUnidad;
+    }
+
+    public void setNombreUnidad(String nombreUnidad) {
+        this.nombreUnidad = nombreUnidad;
+    }
+
+    public UndiadModel(String nombreUnidad) {
+        this.nombreUnidad = nombreUnidad;
+    }
 }
