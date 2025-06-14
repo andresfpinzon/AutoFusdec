5cad703bc5b9766906bc634a3f9de79661881cd6
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-06-14 16:27:09 -0500

    fix: Modifico, para dar uso del remeber del bot, usando el session variables

--- src/main/java/co/com/AutoFusdec/tasks/brigada/BrigadaForm.java
+++ src/main/java/co/com/AutoFusdec/tasks/brigada/BrigadaForm.java
@@ -1,8 +1,10 @@
 package co.com.AutoFusdec.tasks.brigada;
 
 import co.com.AutoFusdec.models.brigadamodel.BrigadaModel;
+import co.com.AutoFusdec.models.usogeneral.SessionVariables;
 import co.com.AutoFusdec.tasks.usogeneral.SeleccionarOpcionDelMenu;
 import co.com.AutoFusdec.tasks.waitTask.Esperar;
+import co.com.AutoFusdec.utils.hooks.useGeneral.NamesGenerater;
 import net.serenitybdd.core.steps.Instrumented;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
@@ -12,7 +14,7 @@ import net.serenitybdd.screenplay.actions.Enter;
 import java.util.List;
 
 import static co.com.AutoFusdec.userinterface.brigada.AccionesBrigadaUI.*;
-import static co.com.AutoFusdec.userinterface.brigada.AccionesBrigadaUI.BTN_CONFIRMAR_BRIGADA;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class BrigadaForm implements Task {
 
@@ -28,19 +30,21 @@ public class BrigadaForm implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+        BrigadaModel brigada = datos.get(0);
+        String nameBrigada = brigada.getNombreBrigada() +" "+ NamesGenerater.nameGenerate().toLowerCase();
         actor.attemptsTo(
                 SeleccionarOpcionDelMenu.conOpcion(BTN_BRIGADA),
                 Esperar.forElement(POPPUP_CREAR),
                 Click.on(BTN_AGREGAR_BRIGADA),
-                Enter.theValue(datos.get(0).getNombreBrigada()).into(INPUT_NOMBRE_BRIGADA),
-                Enter.theValue(datos.get(0).getUbicacionBrigada()).into(INPUT_UBICACION_BRIGADA),
+                Enter.theValue(nameBrigada).into(INPUT_NOMBRE_BRIGADA),
+                Enter.theValue(brigada.getUbicacionBrigada()).into(INPUT_UBICACION_BRIGADA),
                 Click.on(SELECT_COMANDO),
                 Click.on(OPTION_COMANDO),
                 Click.on(SELECT_HORARIO_COMANDO),
                 Click.on(OPTION_HORARIO),
                 Click.on(BTN_CONFIRMAR_BRIGADA),
-                Esperar.forElement(INPUT_BUSCAR_BRIGADA),
-                Enter.theValue(datos.get(0).getNombreBrigada()).into(INPUT_BUSCAR_BRIGADA)
+                Esperar.forElement(INPUT_BUSCAR_BRIGADA)
         );
+        theActorInTheSpotlight().remember(SessionVariables.NombreBrigda.toString(), nameBrigada);
     }
 }
