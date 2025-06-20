163c19a2b505cadc8433a3a7802fff2a4570a261
Andres Pinzon <andrespinzonxd@gmail.com>
2025-06-20 16:38:48 -0500

    fix> correccion suite

--- src/test/java/co/com/AutoFusdec/runners/SuiteAdministrativo/SuiteAdministrativo.java
+++ src/test/java/co/com/AutoFusdec/runners/SuiteAdministrativo/SuiteAdministrativo.java
@@ -1,26 +1,21 @@
 package co.com.AutoFusdec.runners.SuiteAdministrativo;
 
-import co.com.AutoFusdec.runners.AutenticacionRunner;
+import co.com.AutoFusdec.runners.*;
 import co.com.AutoFusdec.runners.runnerBrigada.CrearBrigadaRunner;
 import co.com.AutoFusdec.runners.runnerUnidad.CrearUnidadRunner;
-import co.com.AutoFusdec.userinterface.colegio.CrearColegio;
-import co.com.AutoFusdec.userinterface.comando.CrearComando;
-import co.com.AutoFusdec.userinterface.curso.CrearCurso;
-import co.com.AutoFusdec.userinterface.edicion.CrearEdicion;
-import co.com.AutoFusdec.userinterface.usuario.CrearUsuario;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         AutenticacionRunner.class,
-        CrearUsuario.class,
-        CrearComando.class,
+        CrearUsuarioRunner.class,
+        CrearComandoRunner.class,
         CrearBrigadaRunner.class,
         CrearUnidadRunner.class,
-        CrearColegio.class,
-        CrearCurso.class,
-        CrearEdicion.class,
+        CrearColegioRunner.class,
+        CrearCursoRunner.class,
+        CrearEdicionRunner.class,
 })
 
 public class SuiteAdministrativo {
