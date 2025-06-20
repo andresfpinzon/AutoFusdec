6fd292af4f780aef4844bd170474de943f3b09fa
Andres Pinzon <andrespinzonxd@gmail.com>
2025-06-20 16:19:23 -0500

    refactor pg fusdec

--- src/main/java/co/com/AutoFusdec/userinterface/InicioFusdec.java
+++ src/main/java/co/com/AutoFusdec/userinterface/InicioFusdec.java
@@ -4,7 +4,9 @@ import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.WebDriver;
 
-@DefaultUrl("http://localhost:5173/home")
+//@DefaultUrl("http://localhost:5173/home")
+@DefaultUrl("https://qf5sx04q-5173.use2.devtunnels.ms/")
+
 public class InicioFusdec extends PageObject {
 
     public InicioFusdec(WebDriver driver) {
